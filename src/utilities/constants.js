a1222dead41fcca36189f98409036b45cc3f0a6f
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-06-07 04:03:20 -0300

    Added constant file

---
+++ src/utilities/constants.js
@@ -0,0 +1,48 @@
+
+/**
+ * @module utilities/constants
+ *
+ */
+
+/**
+  * Hardcoded array for age range
+*/
+export const AGE_RANGE = [0, 400];
+/**
+  * Hardcoded weight for age range
+*/
+export const WEIGHT_RANGE = [0, 45];
+/**
+  * Hardcoded height for age range
+*/
+export const HEIGHT_RANGE = [0, 130];
+
+/**
+  * Defines weight unit
+*/
+export const WEIGHT_UNIT = 'kg';
+
+/**
+  * Defines height unit
+*/
+export const HEIGHT_UNIT = 'cm';
+
+/**
+  * Hardcoded array of strings for genders
+*/
+export const GENDER_OPTIONS = [
+  'Androginous',
+  'Agender',
+  'Bigender',
+  'Female',
+  'Male',
+  'Female To Male',
+  'Genderqueer',
+  'Male to Female',
+  'Other',
+  'Pangender',
+  'Third Gender',
+  'Transgender',
+  'Transsexual',
+  'Two-Spirit',
+];

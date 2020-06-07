f89f0b4c50e7496f05bf860bd772e94bff5b2c44
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-06-07 03:31:27 -0300

    Added basic CSS to app vue file

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,42 @@ export default {
 </script>
 
 <style lang="scss">
+
+
+/* ==========================================================================
+   Main Styles
+   ========================================================================== */
+
+html {
+    height: 100%;
+}
+
+body {
+  min-height: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+  font-family: MuseoSans, "Helvetica Neue", Helvetica,Arial, sans-serif;
+  font-size: 13px;
+  text-rendering: optimizeLegibility;
+  -moz-osx-font-smoothing: grayscale;  /* Font smoothing tweaks for MacOSx in Gecko */
+  -webkit-font-smoothing: antialiased;
+  color: #444A59;
+  background-color: #f8f8f8;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+header,
+footer {
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+
 </style>

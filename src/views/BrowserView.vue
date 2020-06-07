beea96ff57351dd54a8c2e6b18f18ad753a37258
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-06-07 04:33:06 -0300

    Implemented a list of Inhabitants with mocked data

--- src/views/BrowserView.vue
+++ src/views/BrowserView.vue
@@ -1,14 +1,46 @@
 <template>
   <div>
-    <main class="browser-view">
-      View
-    </main>
+    <div>
+      <main class="population-page">
+        <div>SIDEBAR</div>
+          <div class="">
+            <div class="found-text">
+              <h4 v-if="dataSource.length > 0"> We have found {{ dataSource.length }} matches!</h4>
+              <h4 v-else> We haven't found any matches &#128547;</h4>
+            </div>
+            <ul class="population-list">
+              <li :class="isSidebarVisible ? 'population-list__item-with-sidebar' : 'population-list__item-no-sidebar'" v-for="inhabitant in dataSource" :key="inhabitant.id">
+                <Inhabitant
+                :inhabitant="inhabitant"
+              />
+              </li>
+            </ul>
+          </div>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
+import Inhabitant from '@/components/Population/Inhabitant.vue';
+import mockedData from '../store/__mockedPopulation__';
+
 export default {
   name: 'BrowserView',
+  components: {
+    Inhabitant,
+  },
+
+  data() {
+    return {
+      isSidebarVisible: true,
+    };
+  },
+  computed: {
+    dataSource() {
+      return mockedData;
+    },
+  },
 };
 </script>
 

2c820b1f979e358b1c92140d272ab9d68d81b381
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-06-07 03:32:05 -0300

    Added base Header structure & CSS

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Header
+    <div class="header-container">
+    <header class="header">
+    </header>
   </div>
 </template>
 
@@ -10,6 +11,33 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+/* ==========================================================================
+   Header
+   ========================================================================== */
+
+.header-container {
+  width: 100%;
+  margin-bottom: 20px;
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.06);
+  top: 0;
+  -webkit-box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.06);
+  padding: 0;
+  z-index: 999;
+  position: fixed;
+  width: 100%;
+  background-color: #ffffff;
+}
+
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  padding-left: 5px;
+  // max-width: 1240px;
+  margin: auto;
+}
 
 </style>

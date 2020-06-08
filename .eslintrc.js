50161f040746355fdceb62dfea46f4d99cc1d9ef
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2020-06-08 02:08:26 -0300

    Added rule exceptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'max-len': 'off',
+    'no-shadow': ['error', { 'allow': ['state'] }],
+    'camelcase': ['error', { 'ignoreDestructuring': true, properties: 'never' }],
   },
   overrides: [
     {
